19e17b91a77b53e3f19f9afd46677e5223761c46
mochisvelas <velasquezmochis@gmail.com>
2021-10-20 16:02:30 -0600

    fin R part

--- HOJAS DE TRABAJO/RIntro/DataManipulation.R
+++ HOJAS DE TRABAJO/RIntro/DataManipulation.R
@@ -2,9 +2,11 @@ install.packages("ggplot2")
 install.packages("dplyr")
 install.packages("DBI")
 install.packages("odbc")
+install.packages("stringr")
 
 library(ggplot2)
 library(dplyr)
+library(stringr)
 library(DBI)
 library(odbc)
 
@@ -15,11 +17,10 @@ con <- dbConnect(odbc(), Driver = "SQL Server",
                  timeout = 5000)
 
 
-
 #### Part b ####
 
 # Exercise i
-dfExamFaculty <- dbGetQuery(con, "select e.*, c.NombreFacultad 
+dfExamFaculty <- dbGetQuery(con, "select e.*, c.NombreFacultad
                     from Fact.Examen e inner join
                     Dimension.Carrera c on (e.sk_carrera = c.sk_carrera)")
 
@@ -46,12 +47,35 @@ dfFacultyMean <- dbGetQuery(con, "select e.*, c.NombreFacultad
                     Dimension.Carrera c on (e.sk_carrera = c.sk_carrera)")
 
 dfTopFacultyMean <- dfFacultyMean %>% group_by(NombreFacultad) %>% summarise(NotaPromedio = mean(NotaTotal)) %>% arrange(desc(NotaPromedio)) %>% slice(1:2)
+
+
 #### Part c ####
 
 # Exercise i
+
 ggplot(dfExamFacultyCount, aes(x="", y=n, fill=NombreFacultad)) +
   geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0)
 
-rm(dfExamCount)
 
+# Exercise ii
+dfCareerMean <- dbGetQuery(con, "select e.*, c.NombreCarrera
+                    from Fact.Examen e inner join
+                    Dimension.Carrera c on (e.sk_carrera = c.sk_carrera)")
+
+dfGroupedCareerMean <- dfCareerMean %>% group_by(NombreCarrera) %>% summarise(NotaPromedio = mean(NotaTotal))
+
+ggplot(dfGroupedCareerMean, aes(x=NombreCarrera, y=NotaPromedio)) +
+  geom_bar(stat="identity") +
+  coord_flip()
+
+# Exercise iii
+dfExamYear <- dbGetQuery(con, "select e.*, f.Year
+                    from Fact.Examen e inner join
+                    Dimension.Fecha f on (e.datekey = f.datekey)")
+
+dfExamYearCount <- dfExamYear %>% count(Year)
+
+ggplot(dfExamYearCount, aes(x=Year, y=n, group=1)) +
+  geom_line() +
+  geom_point()
